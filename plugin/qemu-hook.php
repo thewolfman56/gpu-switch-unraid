30137666d7c4584e28fedd97ff81d8b3c6e4b74c
thewolfman56 <71470070+thewolfman56@users.noreply.github.com>
2026-04-28 22:54:56 -0700

    Refactor qemu-hook.php for improved readability

--- plugin/qemu-hook.php
+++ plugin/qemu-hook.php
@@ -1,15 +1,11 @@
 <?php
-$vm = $argv[1];
-$action = $argv[2];
+$vm=$argv[1]; $action=$argv[2];
+$config=json_decode(file_get_contents("/usr/local/emhttp/plugins/gpu-switch/config.json"),true);
+if(!$config['auto_switch']) exit;
 
-$config = json_decode(file_get_contents("/usr/local/emhttp/plugins/gpu-switch/config.json"), true);
-
-if (!$config['auto_switch']) exit;
-
-if ($vm === $config['vm_name'] && $action === "prepare") {
-    exec("/usr/local/emhttp/plugins/gpu-switch/gpu-switch.sh enter_vm");
+if($action==="prepare"){
+ exec("/usr/local/emhttp/plugins/gpu-switch/gpu-switch.sh enter_vm");
 }
-
-if ($vm === $config['vm_name'] && $action === "release") {
-    exec("/usr/local/emhttp/plugins/gpu-switch/gpu-switch.sh exit_vm");
+if($action==="release"){
+ exec("/usr/local/emhttp/plugins/gpu-switch/gpu-switch.sh exit_vm");
 }
