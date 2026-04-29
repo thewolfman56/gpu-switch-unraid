1607e41940198ccbe6ede2b6293b14cef6eac776
thewolfman56 <71470070+thewolfman56@users.noreply.github.com>
2026-04-28 22:54:24 -0700

    Refactor gpu-switch.sh for improved readability

--- plugin/gpu-switch.sh
+++ plugin/gpu-switch.sh
@@ -1,41 +1,36 @@
 #!/bin/bash
-
 BASE="/usr/local/emhttp/plugins/gpu-switch"
 CONFIG="$BASE/config.json"
 LOG="$BASE/logs/gpu.log"
 STATE="$BASE/state/containers.state"
 MODE="$BASE/state/mode"
 
-log() { echo "$(date '+%F %T') | $1" >> "$LOG"; }
+log(){ echo "$(date '+%F %T') | $1" >> "$LOG"; }
 
-get_json() { jq -r "$1" "$CONFIG"; }
+set_mode(){ echo "$1" > "$MODE"; }
 
-set_mode() { echo "$1" > "$MODE"; }
+save_state(){ docker ps --format '{{.Names}}' > "$STATE"; }
 
-wait_gpu_idle() {
+stop_gpu(){
+  for c in $(jq -r '.containers.gpu[]' "$CONFIG"); do
+    docker stop "$c"
+  done
+}
+
+start_gpu(){
+  for c in $(cat "$STATE" 2>/dev/null); do
+    docker start "$c"
+  done
+}
+
+wait_gpu_idle(){
   for i in {1..30}; do
     ! nvidia-smi | grep -q " C " && return
     sleep 1
   done
 }
 
-stop_gpu() {
-  for c in $(get_json '.containers.gpu[]'); do
-    docker stop "$c"
-  done
-}
-
-start_gpu() {
-  for c in $(cat "$STATE" 2>/dev/null); do
-    docker start "$c"
-  done
-}
-
-save_state() {
-  docker ps --format '{{.Names}}' > "$STATE"
-}
-
-enter_vm() {
+enter_vm(){
   log "Entering VM mode"
   set_mode vm
   save_state
@@ -43,13 +38,13 @@ enter_vm() {
   wait_gpu_idle
 }
 
-exit_vm() {
+exit_vm(){
   log "Returning to Docker mode"
   set_mode docker
   start_gpu
 }
 
 case "$1" in
-  enter_vm) enter_vm ;;
-  exit_vm) exit_vm ;;
+ enter_vm) enter_vm ;;
+ exit_vm) exit_vm ;;
 esac
